a349ebe7f98883ef391dc56b9bbdbe509738aadb
Prim90 <Prim90@users.noreply.github.com>
2018-07-07 18:46:01 +0200

    Przywitaj się z robotem cw 2

---
+++ Pytanie_i_odzpowiedz.py
@@ -0,0 +1,22 @@
+print("Jak masz imię?")
+
+od = "Odpowiedz: "
+
+imie = input(od)
+
+print("Cześć,", imie)
+
+print("A na nazwisko?")
+
+nazwisko = input(od)
+
+print("Miło Cię poznać", imie, nazwisko)
+
+print("W którym roku się urodziłeś?")
+
+data_urodzenia = int(input(od))
+
+print("Masz", 2018 - data_urodzenia,"lata?")
+
+print("Zacy wiek. Dziękuję to już wszystko.")
+
